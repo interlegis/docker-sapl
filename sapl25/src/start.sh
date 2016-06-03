c5500dc8ffc91d9d9271b9d1dbc95bebb807f190
Fabio Rauber <fabiorauber@gmail.com>
2016-06-03 17:10:05 -0300

    Fixes SAPL not writing properly to the zeoserver

--- sapl25/src/start.sh
+++ sapl25/src/start.sh
@@ -10,7 +10,7 @@ trap _terminate SIGTERM SIGINT
 mysqlcheck() {
   # Wait for MySQL to be available...
   COUNTER=10
-  until mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "show databases"; do
+  until mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "show databases" 2>/dev/null; do
     echo "WARNING: MySQL still not up. Trying again..."
     sleep 1
     let COUNTER-=1
@@ -24,17 +24,71 @@ mysqlcheck() {
   if [ "$count" == "0" ]; then
     until mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -e "show databases"; do
       echo "MySQL is unavailable - sleeping"
-      sleep 1
+      sleep 2
     done
     echo "Database is empty. Importing base tables..."
     mysql -h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < $INSTALLDIR/instances/sapl25/Products/ILSAPL/instalacao/sapl.sql && echo "Import done."
-
-    echo "Configuring SAPL..."
-    $INSTALLDIR/instances/sapl25/bin/zopectl run $INSTALLDIR/sapl_create.py && echo "SAPL configured successfully."
   fi 
 }
 
+zopesaplcheck() {
+  if [ ! -f "$INSTALLDIR/.saplcreated" ]; then
+    echo "Configuring SAPL..."
+    $INSTALLDIR/instances/sapl25/bin/zopectl run $INSTALLDIR/sapl_create.py 2>/dev/null && \
+    touch "$INSTALLDIR/.saplcreated" && \
+    echo "SAPL configured successfully."
+  fi 
+}
+
+zeoclientcheck() {
+  if [ "${ZEO_CLIENT,,}" == "true" ]; then
+    dbmain="
+    mount-point /
+    cache-size 5000
+    <zeoclient>
+     server $ZEO_ADDRESS
+     storage 1
+     name zeostorage
+     var \$INSTANCE/var
+   </zeoclient>"
+
+    dbtemp="
+    mount-point /temp_folder
+    container-class Products.TemporaryFolder.TemporaryContainer
+    cache-size 5000
+    <zeoclient>
+     server $ZEO_ADDRESS
+     storage temp
+     name zeostorage
+     var \$INSTANCE/var
+   </zeoclient>"
+
+    zopeconf=$INSTALLDIR/instances/sapl25/etc/zope.conf
+   
+    awk -vcontent="$dbmain" '
+      BEGIN       {p=1}
+      /^<zodb_db main>/    {print;print content;p=0}
+      /^<\/zodb_db>/    {p=1}
+      p' $zopeconf > "$zopeconf.tmp1"
+    
+    awk -vcontent="$dbtemp" '
+      BEGIN       {p=1}
+      /^<zodb_db temporary>/    {print;print content;p=0}
+      /^<\/zodb_db>/    {p=1}
+      p' "$zopeconf.tmp1" > "$zopeconf.tmp2"
+
+    mv "$zopeconf.tmp2" $zopeconf
+    rm "$zopeconf.tmp1"
+
+    #Remove zodb_db documentos
+    sed -i '/<zodb_db documentos>/,/<\/zodb_db>/d' $zopeconf
+
+  fi 
+}
+
+zeoclientcheck
 mysqlcheck
+zopesaplcheck
 
 $INSTALLDIR/instances/sapl25/bin/zopectl start
 $INSTALLDIR/instances/sapl25/bin/zopectl logtail &
