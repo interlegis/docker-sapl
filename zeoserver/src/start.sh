46a3bfc2e4a31ef576f01cf687cab7a26e968572
Fabio Rauber <fabiorauber@gmail.com>
2016-06-03 15:41:17 -0300

    Added temporary storage to the zeoserver container

--- zeoserver/src/start.sh
+++ zeoserver/src/start.sh
@@ -7,6 +7,22 @@ _terminate() {
 
 trap _terminate SIGTERM SIGINT
 
+tempstoragecheck() {
+  if [ "${TEMPSTORAGE,,}" == "true" ]; then
+    
+    zeoconf=$ZEOHOME/etc/zeo.conf
+
+    echo "
+%import tempstorage
+<temporarystorage temp>
+  name temporary storage for sessioning
+</temporarystorage>" >> $zeoconf
+ 
+  fi
+}
+
+tempstoragecheck
+
 $ZEOHOME/bin/zeoctl start
 $ZEOHOME/bin/zeoctl logtail &
 
